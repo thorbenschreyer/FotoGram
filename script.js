14aab7f8f23ba34be70f488e165101bcca688695
Thorben Schreyer <thorben.schreyer@web.de>
2026-01-06 23:07:26 +0100

    implement dialog

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function addImg (imgSrc) {
+let contentDialog = document.getElementById("image-present-dialog")
 
-            
+function openDialog () {
+    let contentDialog = document.getElementById("image-present-dialog")
+    contentDialog.showModal()
+}
+
+function closeDialog () {
+    let contentDialog = document.getElementById("image-present-dialog")
+    contentDialog.close()
 }
